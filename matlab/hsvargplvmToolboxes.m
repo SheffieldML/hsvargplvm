d0df97ce94cb2b0edbdc41e36ac9d79f9851cb16
Neil Lawrence <N.Lawrence@sheffield.ac.uk>
2015-12-30 19:47:01 +0100

    Edits for getting Gpmat compatibility.

--- matlab/hsvargplvmToolboxes.m
+++ matlab/hsvargplvmToolboxes.m
@@ -1,15 +1,4 @@
 % HSVARGPLVMTOOLBOXES Load in the relevant toolboxes for hierarchical sharedvariational gplvm.
 
 importTool('vargplvm');
-importLatest('fgplvm');
-importLatest('gplvm');
-importLatest('netlab');
-importLatest('prior');
-importLatest('optimi');
-importLatest('datasets');
-importTool('mltools');
-importLatest('kern');
-importLatest('ndlutil');
-importLatest('mocap');
-importLatest('gp');
-importLatest('noise');
+vargplvmToolboxes;
